0ea71d2f26a7c3f8f7499a09230505c67898fdf3
Pedro Fluxa <pfluxa@gausscontrol.com>
2021-06-13 11:44:21 -0400

    mapping routines were deprecated (recycled, really) in favor of new SkyMap object.

--- src/Mapping/chealpix_interpolate.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CHEALPIXINTERPOLATEH
-#define _CHEALPIXINTERPOLATEH
-
-void
-chealpix_get_interpolation( int nside, double phi, double theta, int *pix, double *wgt );
-
-#endif
---
+++ src/SkyMap/skymap.hpp
@@ -0,0 +1,58 @@
+/*
+ * skymap.hpp 
+ * 
+ */
+
+// begin include guard
+#ifndef _SKYMAPH
+#define _SKYMAPH
+
+// include healpix related routines
+#include <healpix_base.h>
+
+class SkyMap
+{
+    const int INSUFFCOV = -10;
+    
+    public:
+        SkyMap(int _nside); 
+       ~SkyMap();
+        
+        void accumulate_data
+        (
+            int nsamples, float ra[], float dec[], float pa[],
+            int ndetectors, float detPolAngles[],
+            float data[]
+        );
+        void solve_map(void);
+    
+        int get_nside (void) const { return nside; };
+        int size (void) const { return nPixels; };
+        
+        const float* get_stokes_I(void){ return stokesI; };
+        const float* get_stokes_Q(void){ return stokesQ; };
+        const float* get_stokes_U(void){ return stokesU; };
+        const int* get_hitmap(void){ return hitsMap; };
+        
+    private:
+		
+		int  nside;
+        long nPixels;
+        
+        double* AtA;
+        double* AtD;
+
+		Healpix_Base hpx;
+
+		int* hitsMap;
+        float* stokesI;
+		float* stokesQ;
+		float* stokesU;
+        
+        void allocate_buffers(void);
+        void deallocate_buffers(void);
+                
+};
+
+// end include guard
+#endif 
