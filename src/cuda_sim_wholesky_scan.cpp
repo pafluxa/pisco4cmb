41df123d112148beb191e399820bcc4b1ed86a4d
Pedro Fluxa <pfluxa@gausscontrol.com>
2021-06-13 11:46:04 -0400

    added support for new SkyMap object. Dropped old libmapping routines.

--- src/cuda_sim_wholesky_scan.cpp
+++ src/cuda_sim_wholesky_scan.cpp
@@ -1,6 +1,7 @@
 /**/
 #include <cstdlib>
 #include <cstring>
+#include <chrono>
 #include <fstream>
 #include <cstdlib>
 #include <unistd.h>
@@ -10,15 +11,16 @@
 #include "Bpoint/BPoint.h"
 #include "Polbeam/polbeam.hpp"
 #include "GpuConvolver/gpuconvolver.hpp"
-#include "Mapping/mapping_routines.h"
+#include "SkyMap/skymap.hpp"
 
 // needed to create the point source map
 #include <healpix_base.h>
 #include <pointing.h>
 
-#define NSIDE_SKY (128)
-#define NPIXELS_SKY (12*(NSIDE_SKY)*(NSIDE_SKY))
-#define NSAMPLES (NPIXELS_SKY)
+#define NSIDE_SKY (256)
+#define NSIDE_SKY_OUT (128)
+#define NPIXELS_SKY (12 * (NSIDE_SKY) * (NSIDE_SKY))
+#define NPIXELS_SKY_OUT (12 * (NSIDE_SKY_OUT) * (NSIDE_SKY_OUT))
 /** how to calculate the number of pixels below
 import healpy
 import numpy
@@ -28,19 +30,24 @@ print(healpy.query_disc(nside, (0,0,1), numpy.radians(5)).size)
 **/
 #define NSIDE_BEAM 2048
 #define NPIXELS_BEAM (95484)
-
-/** buffers to store the sky **/
+/** number of time samples = number of pixels because this is a 
+ *  whole sky simulation.
+ **/
+#define NSAMPLES (NPIXELS_SKY)
+/** pointers to buffers storing the sky **/
 float* skyI;
 float* skyQ;
 float* skyU;
 float* skyV;
+/** pointers to buffers storing output maps. **/
+const int* hits;
+const float* mapI;
+const float* mapQ;
+const float* mapU;
 /** buffers so store PSB pointing **/
 float* ra;
 float* dec;
 float* psi;
-/** map-making accumulation matrix (vector) **/
-double* AtA;
-double* AtD;
 /** buffers to simulated data **/
 // detector A
 float* psbDataA;
@@ -51,7 +58,7 @@ float* psbDataB;
  * defined by ra0, dec0. the scan is executed at position angle psi0.
  **/
 void init_wholesky_scan(
-    int nside, double pa, float *ra, float *dec, float *psi);
+    int nside, float pa, float *ra, float *dec, float *psi);
 /**routine to load sky from disk
  **/
 void load_sky_data(std::string path, 
@@ -65,6 +72,9 @@ void load_beam_data(char polFlag, std::string path, PolBeam& beam);
 
 int main(int argc, char** argv )
 {
+    auto start = std::chrono::steady_clock::now();
+    auto stop = std::chrono::steady_clock::now();
+    
     // path to write the output map
     std::string outfilePath;
     // which detectors get projected
@@ -119,6 +129,8 @@ int main(int argc, char** argv )
     // initialize sky object
     load_sky_data(inputSkyPath, skyI, skyQ, skyU, skyV);
     Sky sky(NSIDE_SKY, skyI, skyQ, skyU, skyV);
+    // initialize SkyMap object 
+    SkyMap smap(NSIDE_SKY_OUT);
     // initialize polarized beam object. read beam data from disk
     PolBeam beam(NSIDE_BEAM, NPIXELS_BEAM, 0.0, projDets);
     if(beamsFromGauss) 
@@ -150,73 +162,65 @@ int main(int argc, char** argv )
     CUDACONV::RunConfig cfg;
     cfg.gridSizeX = 512;
     cfg.gridSizeY = 1;
-    cfg.blockSizeX = 32;
+    cfg.blockSizeX = 64;
     cfg.blockSizeY = 1;
-    cfg.ptgPerConv = 8192 * 8;
+    cfg.ptgPerConv = 8192 * 4;
     cfg.pixelsPerDisc = 2000;
     GPUConvolver cconv(scan, cfg);
     cconv.update_sky(&sky);
     cconv.update_beam(&beam);
     // setup detetctor angle arays
-    double detectorAngle[1];
-    double positionAngles[] = {-45, 0, 45};
+    float detectorAngle[1];
+    float positionAngles[] = {-45, 0, 45};
     // every PSB scans the sky at different angles
-    for(double bcpa_deg: positionAngles)
+    for(float bcpa_deg: positionAngles)
     {
         // angles are in degrees. convert.
-        double bcpa = M_PI * (bcpa_deg / 180.0);
+        float bcpa = M_PI * (bcpa_deg / 180.0);
         init_wholesky_scan(NSIDE_SKY, bcpa, ra, dec, psi);
+        start = std::chrono::steady_clock::now();
         // compute convolution for detector A and B of PSB
         cconv.exec_convolution(
             cfg, 
             psbDataA, psbDataB, 
             &scan, &sky, &beam);
+        stop = std::chrono::steady_clock::now();
+        double elapsed = 
+            std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cerr << "elapsed time per convolution: " << elapsed << "ms" << std::endl;
         // project detector data to map-making matrices
         if(projDets == 'a' || projDets == 'p') 
         {
             // project detector a
             detectorAngle[0] = 0;
-            libmapping_project_data_to_matrices
-            (
-                NSAMPLES, 
-                ra, dec, psi,
+            smap.accumulate_data
+            (   NSAMPLES, ra, dec, psi, 
                 1, detectorAngle,
-                psbDataA,
-                NSIDE_SKY,
-                AtA, AtD
+                psbDataA
             );
         }
         if(projDets == 'b' || projDets == 'p') 
         {
             // project detector b
             detectorAngle[0] = M_PI_2;
-            libmapping_project_data_to_matrices
-            (
-                NSAMPLES,
-                ra, dec, psi,
+            smap.accumulate_data
+            (   NSAMPLES, ra, dec, psi, 
                 1, detectorAngle,
-                psbDataB,
-                NSIDE_SKY,
-                AtA, AtD
+                psbDataB
             );
         }
     }
-    std::memset(skyI, 0, sizeof(float)*NPIXELS_SKY);
-    std::memset(skyQ, 0, sizeof(float)*NPIXELS_SKY);
-    std::memset(skyU, 0, sizeof(float)*NPIXELS_SKY);
-    std::memset(skyV, 0, sizeof(float)*NPIXELS_SKY);
-    libmapping_get_IQU_from_matrices
-    (
-        NSIDE_SKY,
-        AtA, AtD,
-        skyI, skyQ, skyU, skyV
-    );
-    for(int i=0; i<NPIXELS_SKY; i++)
+    smap.solve_map();
+    hits = smap.get_hitmap();
+    mapI = smap.get_stokes_I();
+    mapQ = smap.get_stokes_Q();
+    mapU = smap.get_stokes_U();
+    for(int i=0; i < NPIXELS_SKY_OUT; i++)
     {
-        outdata << skyI[i] << " " <<
-                   skyQ[i] << " " <<
-                   skyU[i] << " " <<
-                   skyV[i] << std::endl;
+        outdata << mapI[i] << " " <<
+                   mapQ[i] << " " <<
+                   mapU[i] << " " <<
+                   hits[i] << std::endl;
     }
     outdata.close();
     free_everything();
@@ -226,7 +230,7 @@ int main(int argc, char** argv )
 
 void init_wholesky_scan(
     int nside, 
-    double pa,
+    float pa,
     float* ra, float* dec, float* psi) 
 {
     
@@ -255,12 +259,6 @@ void allocate_everything(void)
     
     psbDataA = (float*)malloc(sizeof(float)*NSAMPLES);
     psbDataB = (float*)malloc(sizeof(float)*NSAMPLES);
-    
-    // zero out map-making accumulation matrices
-    AtA = (double*)malloc(sizeof(double)*9*NPIXELS_SKY);
-    AtD = (double*)malloc(sizeof(double)*3*NPIXELS_SKY);
-    std::memset(AtA, 0, sizeof(double)*9*NPIXELS_SKY);
-    std::memset(AtD, 0, sizeof(double)*3*NPIXELS_SKY);
 }
 
 void free_everything(void) 
@@ -274,9 +272,6 @@ void free_everything(void)
     free(dec);
     free(psi);
     std::cerr << "pointing buffers freed" << std::endl;
-    free(AtA);
-    free(AtD);
-    std::cerr << "AtA and AtD buffers freed" << std::endl;
     free(psbDataA);
     free(psbDataB);
     std::cerr << "data buffers freed" << std::endl;
