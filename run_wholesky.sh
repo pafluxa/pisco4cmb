30d29970b5c5eaab2906f6bcc72d1958023269b3
Pedro Fluxa <pfluxa@gausscontrol.com>
2021-06-15 09:43:39 -0400

    minor change. some code commented out for convenience.

--- run_wholesky.sh
+++ run_wholesky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TAG="test"
+TAG="test_nstreams-3"
 nsideIn=256
-nsideOut=128
+nsideOut=256
 beamnside=2048
 beamfwhm_deg=0.6604
 scan=wholesky 
@@ -16,18 +16,18 @@ cd build || exit 1
 rm -r * || exit 1
 cmake ../ \
   -DCONVOLVER_DISABLECHI=OFF \
-  -DCONVOLVER_OPENMPENABLED=ON \
+  -DCONVOLVER_OPENMPENABLED=OFF \
   -DPOLBEAM_DUMPBEAMS=ON \
-  -DCONVOLVER_DEBUG=ON \
+  -DCONVOLVER_DEBUG=OFF \
   -DMAPPING_DEBUG=ON \
   -DUSE_CUDA=$cuda || exit 1
 make -j4 simulate_wholesky_scan.x || exit 1
 cd ../ || exit 1
 # build beams
-python scripts/fields2beams.py \
-  data/beams/offset_ref/det_a.txt $beamnside || exit 1
-python scripts/fields2beams_cxisco.py \
-  data/beams/offset_ref/det_b.txt $beamnside || exit 1
+#python scripts/fields2beams.py \
+#  data/beams/offset_ref/det_a.txt $beamnside || exit 1
+#python scripts/fields2beams_cxisco.py \
+#  data/beams/offset_ref/det_b.txt $beamnside || exit 1
 cp data/beams/offset_ref/hpx_tilde_beams_det_a.txt \
   "output/beam_a.txt" || exit 1
 cp data/beams/offset_ref/hpx_tilde_beams_det_b.txt \
@@ -49,7 +49,7 @@ time "./build/"$program \
   -o "output/MAPS_det_"$det"_scan_"$scan".map" \
   -t f \
   -a output/beam_a.txt \
-  -b output/beam_b.txt 
+  -b output/beam_b.txt || exit 1
 #   > "output/STDOUT_det_"$det"_scan_"$scan".txt" || exit 1
 # move dumped beams to output folder
 mv dump_detector_a.txt output/
